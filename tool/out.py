4a9870523cf7f26728276174a96eafc36794f55d
iusethemouse <iusethemouse@gmail.com>
2021-09-03 16:17:29 +0200

    correct Kappa model

--- tool/out.py
+++ tool/out.py
@@ -100,122 +100,122 @@ class Mln_dynamics(Model):
 
 		r_10 = Reaction(
 				name = "r_10",
-				rate = betaU,
-				reactants = {US0: 1, AI1: 1},
-				products = {AI0: 1, AI1: 1}
+				rate = betaA,
+				reactants = {AS1: 1, AI2: 1},
+				products = {AI1: 1, AI2: 1}
 			)
 
 		r_11 = Reaction(
 				name = "r_11",
-				rate = betaU,
-				reactants = {US1: 1, AI0: 1},
-				products = {AI1: 1, AI0: 1}
+				rate = betaA,
+				reactants = {AS2: 1, AI1: 1},
+				products = {AI2: 1, AI1: 1}
 			)
 
 		r_12 = Reaction(
 				name = "r_12",
 				rate = betaU,
-				reactants = {US0: 1, AI2: 1},
-				products = {AI0: 1, AI2: 1}
+				reactants = {US0: 1, AI1: 1},
+				products = {AI0: 1, AI1: 1}
 			)
 
 		r_13 = Reaction(
 				name = "r_13",
 				rate = betaU,
-				reactants = {US2: 1, AI0: 1},
-				products = {AI2: 1, AI0: 1}
+				reactants = {US1: 1, AI0: 1},
+				products = {AI1: 1, AI0: 1}
 			)
 
 		r_14 = Reaction(
 				name = "r_14",
-				rate = lamb,
-				reactants = {US0: 1, AI1: 1},
-				products = {AS0: 1, AI1: 1}
+				rate = betaU,
+				reactants = {US0: 1, AI2: 1},
+				products = {AI0: 1, AI2: 1}
 			)
 
 		r_15 = Reaction(
 				name = "r_15",
-				rate = lamb,
-				reactants = {US1: 1, AI0: 1},
-				products = {AS1: 1, AI0: 1}
+				rate = betaU,
+				reactants = {US2: 1, AI0: 1},
+				products = {AI2: 1, AI0: 1}
 			)
 
 		r_16 = Reaction(
 				name = "r_16",
-				rate = lamb,
-				reactants = {US0: 1, AI2: 1},
-				products = {AS0: 1, AI2: 1}
+				rate = betaU,
+				reactants = {US1: 1, AI2: 1},
+				products = {AI1: 1, AI2: 1}
 			)
 
 		r_17 = Reaction(
 				name = "r_17",
-				rate = lamb,
-				reactants = {US2: 1, AI0: 1},
-				products = {AS2: 1, AI0: 1}
+				rate = betaU,
+				reactants = {US2: 1, AI1: 1},
+				products = {AI2: 1, AI1: 1}
 			)
 
 		r_18 = Reaction(
 				name = "r_18",
 				rate = lamb,
-				reactants = {US1: 1, AI2: 1},
-				products = {AS1: 1, AI2: 1}
+				reactants = {US0: 1, AI1: 1},
+				products = {AS0: 1, AI1: 1}
 			)
 
 		r_19 = Reaction(
 				name = "r_19",
 				rate = lamb,
-				reactants = {US2: 1, AI1: 1},
-				products = {AS2: 1, AI1: 1}
+				reactants = {US1: 1, AI0: 1},
+				products = {AS1: 1, AI0: 1}
 			)
 
 		r_20 = Reaction(
 				name = "r_20",
 				rate = lamb,
-				reactants = {US0: 1, AS1: 1},
-				products = {AS0: 1, AS1: 1}
+				reactants = {US0: 1, AI2: 1},
+				products = {AS0: 1, AI2: 1}
 			)
 
 		r_21 = Reaction(
 				name = "r_21",
 				rate = lamb,
-				reactants = {US1: 1, AS0: 1},
-				products = {AS1: 1, AS0: 1}
+				reactants = {US2: 1, AI0: 1},
+				products = {AS2: 1, AI0: 1}
 			)
 
 		r_22 = Reaction(
 				name = "r_22",
 				rate = lamb,
-				reactants = {US0: 1, AS2: 1},
-				products = {AS0: 1, AS2: 1}
+				reactants = {US0: 1, AS1: 1},
+				products = {AS0: 1, AS1: 1}
 			)
 
 		r_23 = Reaction(
 				name = "r_23",
 				rate = lamb,
-				reactants = {US2: 1, AS0: 1},
-				products = {AS2: 1, AS0: 1}
+				reactants = {US1: 1, AS0: 1},
+				products = {AS1: 1, AS0: 1}
 			)
 
 		r_24 = Reaction(
 				name = "r_24",
 				rate = lamb,
-				reactants = {US1: 1, AS2: 1},
-				products = {AS1: 1, AS2: 1}
+				reactants = {US0: 1, AS2: 1},
+				products = {AS0: 1, AS2: 1}
 			)
 
 		r_25 = Reaction(
 				name = "r_25",
 				rate = lamb,
-				reactants = {US2: 1, AS1: 1},
-				products = {AS2: 1, AS1: 1}
+				reactants = {US2: 1, AS0: 1},
+				products = {AS2: 1, AS0: 1}
 			)
 
 		self.add_reaction([r_0 , r_1 , r_2 , r_3 , r_4 , r_5 , r_6 , r_7 , r_8 , r_9 , r_10 , r_11 , r_12 , r_13 , r_14 , r_15 , r_16 , r_17 , r_18 , r_19 , r_20 , r_21 , r_22 , r_23 , r_24 , r_25] ) 
 
-		self.timespan(numpy.linspace(0, 5, 100))
+		self.timespan(numpy.linspace(0, 30, 600))
 
 def run_sim(model):
-	results = model.run(number_of_trajectories=100)
+	results = model.run(number_of_trajectories=1000)
 	trajectory = results.average_ensemble()
 	plt.figure()
 	trajectories = [[], [], []]
@@ -227,5 +227,5 @@ def run_sim(model):
 	plt.plot(trajectory["time"], trajectories[1], "g", label="AS")
 	plt.plot(trajectory["time"], trajectories[2], "r", label="US")
 	plt.legend(loc="upper right")
-	plt.annotate(text=f"n = 100", xy=(0.1, 0.9), xycoords="axes fraction")
+	plt.annotate(text=f"n = 1000", xy=(0.1, 0.9), xycoords="axes fraction")
 	plt.show()
